55e7f1ed24b41d62285bf47ef2d5bc45e72e55ee
musiccity365 <m_sackstein@yahoo.com>
2021-05-05 22:53:24 -0400

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const carsURL = baseURL + '/cars'
 const list = document.getElementById("car-list")
 
 const form = document.getElementById("car-form")
-const
+const makeInput = document.getElementById("car-make")
