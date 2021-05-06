be31d66ba1af074a884be6dad56906449fe97565
musiccity365 <m_sackstein@yahoo.com>
2021-05-06 00:46:50 -0400

    create constructor objects in car.js

--- src/car.js
+++ src/car.js
@@ -1,3 +1,23 @@
 class Car {
 
+    static all = [];
+
+    // NEW CONTAINER FOR CAR-LIST
+    static container = document.getElementById('car-list')
+
+    constructor({
+        id,
+        make,
+        model,
+        year,
+        mileage,
+        originID
+    }) {
+        this.id = id
+        this.make = make
+        this.model = model
+        this.year = year
+        this.mileage = mileage
+        this.originID = originID
+    }
 }
