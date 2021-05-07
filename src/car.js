44da1a2fba20713654c993320eb4fc07bfd8f2e6
musiccity365 <m_sackstein@yahoo.com>
2021-05-06 22:39:23 -0400

    --- Auto-Git Commit ---

--- src/car.js
+++ src/car.js
@@ -19,5 +19,20 @@ class Car {
         this.year = year
         this.mileage = mileage
         this.originID = originID
+
+        this.element.addEventListener('click', this.handleCarClick)
+
+        Car.all.push(this)
+    }
+
+    render() {
+        this.element.innerHTML = `
+        <div data-id="${this.id}">
+            $<strong class="make">${this.make}</strong>
+            $<span class="model">${this.model}</span>
+            $<span class="year">${this.year}</span>
+            $<span class="mileage">${this.mileage}</span>
+        `
+        return this.element
     }
 }
